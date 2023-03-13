6fc3587d2165dc82e350fce413e26d499fc89624
Mukesh-Khemani <mukeshkhemani222@gmail.com>
2023-03-13 22:25:16 +0530

    Symmetric Tree

--- Symmetric_Tree.cpp
+++ Symmetric_Tree.cpp
@@ -1,19 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+/*
+
+INTUITION:
+The idea is to write a recursive function issymmetric() that takes two trees as 
+an argument and returns true if trees are the mirror and false if trees are not 
+mirrored. The issymmetric() function recursively checks two roots and subtrees 
+under the root.
+
+For two trees to be symmetric, the following conditions must be true:
+-> Their root node's key must be same
+-> left subtree of left tree and right subtree of right tree have to be symmetric
+-> right subtree of left tree and left subtree of right tree have to be symmetric
+
+*/
+
 class Solution {
-public:
-    bool issymmetric(TreeNode* p,TreeNode* q){
-        if(p==NULL && q==NULL) return 1;
-        if(p && q && p->val==q->val){
-            return (issymmetric(p->right,q->left) && 
-            issymmetric(p->left,q->right));
-        }
-        return 0;
+    bool issymmetric(TreeNode* p, TreeNode* q){
+        if(p==NULL || q==NULL) return p==q;
+        if(p->val!=q->val) return false;
+        return issymmetric(p->left,q->right) && 
+        issymmetric(p->right,q->left);
     }
+public:
     bool isSymmetric(TreeNode* root) {
-        return issymmetric(root->right,root->left);
+        return issymmetric(root->left,root->right);
     }
 };
+
 int main(){
     return 0;
 }
