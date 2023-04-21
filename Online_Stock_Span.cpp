9387de8809145860cbead3b163c40ed099488875
Mukesh-Khemani <mukeshkhemani222@gmail.com>
2023-04-21 22:49:22 +0530

    comments added

--- Online_Stock_Span.cpp
+++ Online_Stock_Span.cpp
@@ -17,7 +17,7 @@ while traversing from the last and return the answer in the end.
 The above approach traverses through the same elements for calculating
 the span for each stock price. This makes it redundant and more time taking.
 So lets use the concept of Monotonic stack which stores the elements in
-descending order along with the record of the span of the last stock price
+ascending order along with the record of the span of the last stock price
 as well. This makes the time complexity O(1).
 
 */
